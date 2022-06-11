67785b6f4136696a32ba651ff65ea7a81d80a86c
marianives <m.gallo.inf20@stud.bbbaden.ch>
2022-06-11 18:04:13 +0200

    responsive update

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,12 @@ import maria from "../images/bild.png"
 const Home = () => {
     return (
         <div className='home'>
-            <div className='divone'>
-                <img src={maria} alt="Maria-Nives" className='bild '></img>
-                <h1 className='mntitle'> Maria-Nives <br /> Gallo</h1>
-                <p className='portfolio'>In Ausbildung als <br />
-                    Informatikerin Applikationsentwicklung   <br />
-                    Alte Kanti Aarau / BBBaden</p>
-            </div>
+            <h1 className='mntitle'> Maria-Nives <br /> Gallo</h1>
+            <p className='portfolio'>In Ausbildung als <br />
+                Informatikerin Applikationsentwicklung   <br />
+                Alte Kanti Aarau / BBBaden</p>
+            <img src={maria} alt="Maria-Nives" className='bild '></img>
+
         </div>
     )
 }
