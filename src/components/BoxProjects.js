15d849a04d5543e3e241cdb8e436b5c1e59326f2
marianives <m.gallo.inf20@stud.bbbaden.ch>
2022-06-11 22:18:39 +0200

    skills and projects finished

--- src/components/BoxProjects.js
+++ src/components/BoxProjects.js
@@ -17,6 +17,23 @@ export default function App() {
             }}
         >
 
+            <h1 className='titlesprachen'>Projects</h1>
+            <p className='project'>
+                Gerne können Sie meine wichtigsten Portfolios<br />
+                &#40;8 insgesamt&#41; des Lernateliers in dieser Sammlung ansehen: <br />
+                <a href='https://portfolio.bbbaden.ch/view/view.php?t=e15PZfdOlrnyR76V4bkN' className='link' target="_blank">
+                    Portfolios Mahara</a>
+            </p>
+            <p className='project'>
+                Meine Projekte, wie diese Website, können Sie auf GitHub finden: <br />
+                <a href='https://github.com/marianives?tab=repositories' className='link' target="_blank">GitHub Profile</a>
+            </p>
+
+            <p className='project'>
+                In meiner Freizeit nehme ich an Online-Kursen teil und vertiefe mich mit den neugelernten Programmiersprachen oder lerne neue kennen: <br />
+                <a href='https://open.hpi.de/courses?q=&channel=&lang=&topic=Programming&level=' className='link' target="_blank">Online Selbstlernkurse</a>
+            </p>
         </BoxProjects>
+
     )
 }
