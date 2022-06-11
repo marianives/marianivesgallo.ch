d6d5e431898e2ed0aaa46e5a6d44e4f150719eac
marianives <m.gallo.inf20@stud.bbbaden.ch>
2022-06-11 17:42:02 +0200

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import maria from "../images/bild.png"
-//            <div className='portfolio'><p >In Ausbildung als <br />
-//Informatikerin Applikationsentwicklung   <br />
-//Alte Kanti Aarau / BBBaden</p>
-//</div>
+
 
 const Home = () => {
     return (
         <div className='home'>
-            <div><img src={maria} alt="Maria-Nives" className='bild '></img></div>
-            <div>
+            <div className='divone'>
+                <img src={maria} alt="Maria-Nives" className='bild '></img>
                 <h1 className='mntitle'> Maria-Nives <br /> Gallo</h1>
+                <p className='portfolio'>In Ausbildung als <br />
+                    Informatikerin Applikationsentwicklung   <br />
+                    Alte Kanti Aarau / BBBaden</p>
             </div>
         </div>
-
     )
 }
 
