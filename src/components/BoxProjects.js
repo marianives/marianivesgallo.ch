ba74c812d92808b548adbebe0034a7e1476f254a
marianives <m.gallo.inf20@stud.bbbaden.ch>
2022-06-11 22:20:09 +0200

    warning fixed

--- src/components/BoxProjects.js
+++ src/components/BoxProjects.js
@@ -21,17 +21,17 @@ export default function App() {
             <p className='project'>
                 Gerne können Sie meine wichtigsten Portfolios<br />
                 &#40;8 insgesamt&#41; des Lernateliers in dieser Sammlung ansehen: <br />
-                <a href='https://portfolio.bbbaden.ch/view/view.php?t=e15PZfdOlrnyR76V4bkN' className='link' target="_blank">
+                <a href='https://portfolio.bbbaden.ch/view/view.php?t=e15PZfdOlrnyR76V4bkN' className='link' target="_blank" rel="noreferrer">
                     Portfolios Mahara</a>
             </p>
             <p className='project'>
                 Meine Projekte, wie diese Website, können Sie auf GitHub finden: <br />
-                <a href='https://github.com/marianives?tab=repositories' className='link' target="_blank">GitHub Profile</a>
+                <a href='https://github.com/marianives?tab=repositories' className='link' target="_blank" rel="noreferrer">GitHub Profile</a>
             </p>
 
             <p className='project'>
                 In meiner Freizeit nehme ich an Online-Kursen teil und vertiefe mich mit den neugelernten Programmiersprachen oder lerne neue kennen: <br />
-                <a href='https://open.hpi.de/courses?q=&channel=&lang=&topic=Programming&level=' className='link' target="_blank">Online Selbstlernkurse</a>
+                <a href='https://open.hpi.de/courses?q=&channel=&lang=&topic=Programming&level=' className='link' target="_blank" rel="noreferrer">Online Selbstlernkurse</a>
             </p>
         </BoxProjects>
 
