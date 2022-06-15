c66e94064038f71110d51f8630575ce6ad2150f8
marianives <m.gallo.inf20@stud.bbbaden.ch>
2022-06-15 12:19:18 +0200

    responsive changes

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -6,30 +6,24 @@ import gym from "../images/gym.jpg"
 const Aboutme = () => {
   return (
     <div className='aboutme'>
-
       <h1 className='titelaboutme'>About me</h1>
-
-      <div className='hobby'>
-        
+      <div className='hobby'>     
         <div className='hobbys'>
           <p className='hobbyname'>
             &#8226; Ballet - Seit 14 Jahren
           </p>
           <p className='hobbyname'>
-            &#8226;Hip-Hop - Seit 9 Jahren
+            &#8226;HipHop - Seit 9 Jahren
           </p>
           <p className='hobbyname'>
-            &#8226;  Fintess - Seit ein Jahr
+            &#8226;  Fintess - Seit einem Jahr
           </p>
         </div>
-
       </div>
       <div className='images'>
-
         <img src={ballet} alt="ballet" className='bildone'></img>
         <img src={hiphop} alt="hiphop" className='bildtwo'></img>
         <img src={gym} alt="gym" className='bildthree'></img>
-
       </div>
     </div>
   )
