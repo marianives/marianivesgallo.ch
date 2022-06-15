4ab449faabafaeeb646c32138720454990554d6e
marianives <m.gallo.inf20@stud.bbbaden.ch>
2022-06-15 21:23:10 +0200

    finish design

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,38 +9,38 @@ const Skills = () => {
       <div className='boxes'>
         <div className='boxsprachen'>
           <p className='titlesprachenn'>Skills</p>
-          <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
-            <p className='sprachen'>Java</p>
-          </a>
-          <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
-            <p className='sprachen'>HTML / CSS</p>
-          </a>
-          <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
-            <p className='sprachen'>C# / Windows Forms</p>
-          </a>
-          <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
-            <p className='sprachen'>Linux</p>
-          </a>
-          <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
-            <p className='sprachen'>MySQL / Datenbanken</p>
-          </a>
-          <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
-            <p className='sprachen'>Arduino</p>
-          </a>
-          <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
-            <p className='sprachen'>Microsoft Office</p>
-          </a>
-          <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
-            <p className='sprachen'>React.js</p>
-          </a>
-          <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
-            <p className='sprachen'>Algorithmen entwerfn</p>
-          </a>
+            <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
+              <p className='sprachen'>Java</p>
+            </a>
+            <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
+              <p className='sprachen'>HTML / CSS</p>
+            </a>
+            <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
+              <p className='sprachen'>C# / Windows Forms</p>
+            </a>
+            <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
+              <p className='sprachen'>Linux</p>
+            </a>
+            <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
+              <p className='sprachen'>MySQL / Datenbanken</p>
+            </a>
+            <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
+              <p className='sprachen'>Arduino</p>
+            </a>
+            <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
+              <p className='sprachen'>Microsoft Office</p>
+            </a>
+            <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
+              <p className='sprachen'>React.js</p>
+            </a>
+            <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
+              <p className='sprachen'>Algorithmen entwerfen</p>
+            </a>
         </div>
         <div className='boxprojects'>
           <BoxProjects></BoxProjects>
         </div>
-      </div>
+      </div >
 
 
     </div >
