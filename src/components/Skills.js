f55409bbdd1e13ee8ec3b5efeca2779106927917
marianives <m.gallo.inf20@stud.bbbaden.ch>
2022-06-29 11:53:28 +0200

    updates in skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,27 +12,31 @@ const Skills = () => {
             <a href='https://portfolio.bbbaden.ch/view/view.php?t=uiaHDgd4oy08cjf7VYEw' className='portfoliolink' target="_blank" rel="noreferrer">
               <p className='sprachen'>Java</p>
             </a>
-            <a href='https://portfolio.bbbaden.ch/user/m-gallo-inf20/bbbaden-rundgang-1' className='portfoliolink' target="_blank" rel="noreferrer">
-              <p className='sprachen'>HTML / CSS</p>
-            </a>
-            <a href='https://portfolio.bbbaden.ch/user/m-gallo-inf20/modul-404-auftrag-ka-404-9923-io' className='portfoliolink' target="_blank" rel="noreferrer">
-              <p className='sprachen'>Windows Forms</p>
-            </a>
-            <a href='https://portfolio.bbbaden.ch/user/m-gallo-inf20/virtuelle-text-welt' className='portfoliolink' target="_blank" rel="noreferrer">
-              <p className='sprachen'>C#</p>
-            </a>
-            <a href='https://portfolio.bbbaden.ch/user/m-gallo-inf20/modul-105-ddl' className='portfoliolink' target="_blank" rel="noreferrer">
-              <p className='sprachen'>MySQL / Datenbanken</p>
-            </a>
-            <a href='https://portfolio.bbbaden.ch/user/m-gallo-inf20/arduino-projekt-modul-242' className='portfoliolink' target="_blank" rel="noreferrer">
-              <p className='sprachen'>Arduino / C++</p>
-            </a>
             <a href='https://portfolio.bbbaden.ch/user/m-gallo-inf20/modul-122-swiss-lotto' className='portfoliolink' target="_blank" rel="noreferrer">
               <p className='sprachen'>Powershell</p>
             </a>
             <a href='https://portfolio.bbbaden.ch/user/m-gallo-inf20/1306-react-portfolio-website' className='portfoliolink' target="_blank" rel="noreferrer">
               <p className='sprachen'>React.js</p>
             </a>
+            <a href='https://portfolio.bbbaden.ch/user/m-gallo-inf20/modul-404-auftrag-ka-404-9923-io' className='portfoliolink' target="_blank" rel="noreferrer">
+              <p className='sprachen'>Windows Forms</p>
+            </a>
+            <a href='https://portfolio.bbbaden.ch/user/m-gallo-inf20/arduino-projekt-modul-242' className='portfoliolink' target="_blank" rel="noreferrer">
+              <p className='sprachen'>Arduino / C++</p>
+            </a>
+            <a href='https://portfolio.bbbaden.ch/user/m-gallo-inf20/virtuelle-text-welt' className='portfoliolink' target="_blank" rel="noreferrer">
+              <p className='sprachen'>C#</p>
+            </a>
+            <a href='https://portfolio.bbbaden.ch/user/m-gallo-inf20/bbbaden-rundgang-1' className='portfoliolink' target="_blank" rel="noreferrer">
+              <p className='sprachen'>HTML / CSS</p>
+            </a>        
+            <a href='https://portfolio.bbbaden.ch/user/m-gallo-inf20/modul-105-ddl' className='portfoliolink' target="_blank" rel="noreferrer">
+              <p className='sprachen'>MySQL / Datenbanken</p>
+            </a>
+            <p className='sprachen'>Am lernen: Python, JavaScript</p>
+            <p className='sprachen'>Vertiefung in React</p>
+           
+            
         </div>
         <div className='boxprojects'>
           <BoxProjects></BoxProjects>
